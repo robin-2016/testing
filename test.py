885f88c7a725c8a68e150442d868c9afb7800689
robin-2016 <xuhongbin3241@gmail.com>
2016-07-03 06:40:21 +0800

    add time

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 #_*_coding:utf-8_*_
-import os,sys,pprint,platform
+import os,sys,pprint,platform,time
 print os.getcwd()
 print ("hello world!")
 pprint.pprint(sys.path)
 print (platform.architecture())
+print time.strftime('%Y-%m-%d %X',time.localtime(time.time()))
