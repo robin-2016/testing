9f31a36ff2031f7aa92410d134434b09ad46f1ef
robin-2016 <xuhongbin3241@gmail.com>
2016-07-06 03:55:39 -0400

    mod dir

--- flask-test.py
+++ flask-test.py
@@ -1,7 +1,17 @@
 #_*_coding:utf-8_*_
 import os,sys,pprint,platform,time
-print os.getcwd()
-print ("hello world!")
-pprint.pprint(sys.path)
-print (platform.architecture())
-print time.strftime('%Y-%m-%d %X',time.localtime(time.time()))
+from flask import Flask
+#print os.getcwd()     #打印工作的目录
+#pprint.pprint(sys.path)
+#print (platform.architecture())
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+	data = {}
+	data['name'] = "Hello robin !"
+	data['time_now'] = time.strftime('%Y-%m-%d %X',time.localtime(time.time()))
+	return (data['name'] + data['time_now'])
+if __name__ == "__main__":
+	app.run(host='0.0.0.0',port=80)
