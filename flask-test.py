428479d01d41821c9d011bc017e3cb66f65b619d
robin-2016 <xuhongbin3241@gmail.com>
2016-07-07 03:08:33 -0400

    finish weather

--- flask-test.py
+++ flask-test.py
@@ -12,10 +12,7 @@ def hello():
 	data = {}
 	data['name'] = "Hello Robin !"
 	data['time_now'] = time.strftime('%Y-%m-%d %X',time.localtime(time.time()))
-	return (data['name'] + data['time_now'])
-
-@app.route("/wealth/",methods=['GET','POST'])
-def wealth():
+	#wealth
 	url = 'http://apis.baidu.com/apistore/weatherservice/cityid?cityid=101120201'
 	req = urllib2.Request(url)
 	req.add_header("apikey", "1de9b3bcea0804d4977589ee9ebf3ce3")
@@ -23,9 +20,8 @@ def wealth():
 	content = resp.read()
 	if(content):
 		cc = (content.decode("unicode_escape")).encode("utf-8")
-    		dict_wealth = eval(cc)
-    		city = dict_wealth["retData"]["city"]
-    		time = dict_wealth["retData"]["time"]
-		return city + time
+    		wealth = eval(cc)
+		data['wealth'] = wealth['retData']
+	return data['name'] + data['time_now'] + data['wealth']['city'] + data['wealth']['time'] + data['wealth']['weather'] + "最低气温：" + data['wealth']['l_tmp'] + "最高气温：" + data['wealth']['h_tmp'] + data['wealth']['WD'] + data['wealth']['WS'] + data['wealth']['sunrise'] + data['wealth']['sunset']
 if __name__ == "__main__":
 	app.run(host='0.0.0.0',port=80)
