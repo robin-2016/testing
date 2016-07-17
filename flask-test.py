7f14ebccc213bbef351e5815b3c71f762213771a
robin-2016 <xuhongbin3241@gmail.com>
2016-07-17 21:32:34 +0800

    mod template

--- flask-test.py
+++ flask-test.py
@@ -1,9 +1,11 @@
 #_*_coding:utf-8_*_
 import time,sys,urllib,urllib2,json
-from flask import Flask
+from flask import Flask,render_template
 #print os.getcwd()     #打印工作的目录
 #pprint.pprint(sys.path)
 #print (platform.architecture())
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 app = Flask(__name__)
 
@@ -19,9 +21,9 @@ def hello():
 	resp = urllib2.urlopen(req)
 	content = resp.read()
 	if(content):
-		cc = (content.decode("unicode_escape")).encode("utf-8")
-    		wealth = eval(cc)
-		data['wealth'] = wealth['retData']
-	return render_template('template1.html',data=data,wealth=data['wealth'])
+		cc = eval(content)
+		city = ((cc['retData']['city']).decode("unicode_escape")).encode("utf-8")
+		wt = ((cc['retData']['weather']).decode("unicode_escape")).encode("utf-8")
+	return render_template('template1.html',data=data,city=city,wt=wt)
 if __name__ == "__main__":
 	app.run(host='0.0.0.0',port=80)
